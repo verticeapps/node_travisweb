b338483bf163fb39055d7aa07cf11646c7165550
lislis <mail@lislis.de>
2016-03-22 18:33:04 +0100

    update org landing
    remove merge conflict

--- app/components/dashboard-row.js
+++ app/components/dashboard-row.js
@@ -1,11 +1,7 @@
 import Ember from 'ember';
 import { githubCommit as githubCommitUrl } from 'travis/utils/urls';
 import config from 'travis/config/environment';
-<<<<<<< HEAD
 import { hasAdminPermission, hasPushPermission } from 'travis/utils/permission';
-=======
-import Permissions from 'travis/mixins/permissions';
->>>>>>> add permissions mixin to dashboard row
 
 export default Ember.Component.extend(Permissions, {
   tagName: 'li',
